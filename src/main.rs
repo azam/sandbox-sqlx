587cb089daf89276388202f35a3e62d50401b577
azam <azamshul@gmail.com>
2023-10-02 16:46:10 +0900

    Implemented pool type agnostic Repository

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
 use sqlx::Sqlite;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    start().await
+}
 
 #[derive(Debug, sqlx::FromRow)]
 struct User {
@@ -6,12 +14,38 @@ struct User {
     name: String,
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[async_trait]
+trait Repository: std::fmt::Debug {
+    async fn list(&self) -> Vec<User>;
+}
+
+#[derive(Debug, Clone)]
+struct State {
+    repo: Arc<dyn Repository>, // Cannot use box because it requires Clone trait
+}
+
+#[derive(Debug, Clone)]
+struct SqliteRepository {
+    pool: sqlx::SqlitePool,
+}
+
+#[async_trait]
+impl Repository for SqliteRepository {
+    async fn list(&self) -> Vec<User> {
+        let users = sqlx::query_as::<Sqlite, User>("SELECT * FROM \"users\"")
+            .fetch_all(&self.pool)
+            .await
+            .unwrap();
+        return users;
+    }
+}
+
+async fn start() -> Result<(), Box<dyn std::error::Error>> {
     let pool = sqlx::SqlitePool::connect("sqlite:sandbox.db").await?;
-    let users: Vec<User> = sqlx::query_as::<Sqlite, User>("SELECT * FROM \"users\"")
-        .fetch_all(&pool)
-        .await?;
+    let state = State {
+        repo: Arc::new(SqliteRepository { pool }),
+    };
+    let users = state.repo.list().await;
     println!("{:?}", users);
     return Ok(());
 }
